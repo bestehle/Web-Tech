d5eddda283fa2625b4b37c81403e75e9b2553e75
unknown <chritte@.(none)>
2013-01-15 23:51:54 +0100

    Add finish JSP Project

---
+++ Seapal_JSP/forms/trip_info_insertJS.js
@@ -0,0 +1,152 @@
+$(function() {
+
+	window.onload = function(){
+		new JsDatePick({
+			useMode:2,
+			target:"startdatum",
+			dateFormat:"%Y-%m-%d"
+		});
+		new JsDatePick({
+			useMode:2,
+			target:"endedatum",
+			dateFormat:"%Y-%m-%d"
+		});
+	};
+
+
+	$('input').keyup(function(e) {
+		if (e.keyCode == 13) {//Enter is pressed
+			readform();
+		}
+	});    document.getElementById("saveButton").onclick = readform;
+
+});
+
+
+function getParam(variable){ 
+     var query = window.location.search.substring(1);  
+     var vars = query.split("&"); 
+      for (var i=0;i<vars.length;i++) {   
+            var pair = vars[i].split("=");  
+            if(pair[0] == variable){
+            	return pair[1].replace(/%20/g, ' ');
+        	}
+       }       
+       return(false);
+}
+
+
+function readform() {
+	var startdatum;
+	var endedatum;
+	var startzeit;
+	var endezeit;
+	var timePattern = new RegExp("^([01]\\d|2[0-3]):?([0-5]\\d):?([0-5]\\d)$");
+	
+	var data = {};
+	var all = $('#tripTableInput input');	for (var i = 0; i < all.size(); i++) {
+		if(all[i].name == 'startdatum' || all[i].name == 'startzeit' || all[i].name == 'endedatum' ||
+				all[i].name == 'endezeit' || all[i].name == 'dauer') {
+			
+		} else {
+			data[all[i].name] = all[i].value;
+		}
+		if(all[i].name == 'trip_title') {
+			if(all[i].value.replace(/\s/g,"") == "") {
+				alert("Bitte einen Namen fürr den Trip angeben");
+				return;
+			}
+			continue;
+		}
+		if(all[i].name == 'motor') {
+			if(!all[i].value.replace(/\s/g,"") == "") {
+				if ( isNaN(all[i].value) || all[i].value < 0  ) {
+					alert("Für " + "'" + all[i].name + "'" + " wurde ein falscher Wert angegeben\n" + 
+						"Bitte eine korrekte Zahl eingeben");
+					return;
+				}
+			}
+			continue;	
+		}		if(all[i].name == 'tank') {
+			if(!all[i].value.replace(/\s/g,"") == "") {
+				if ( isNaN(all[i].value) || all[i].value < 0 || all[i].value > 100 ) {
+					alert("Für " + "'" + all[i].name + "'" + " wurde ein falscher Wert angegeben\n" + 
+							"Bitte eine korrekte Zahl zwischen 0-100 eingeben");
+						return;				}
+			}
+			continue;	
+		}
+		
+		if(all[i].name == 'startdatum') {
+			if(all[i].value.replace(/\s/g,"") == "") {
+				alert("Bitte ein Start Datum angeben");
+				return;
+			}
+			startdatum = all[i].value;
+			continue;
+		}
+			
+		if(all[i].name == 'endedatum') {
+			if(all[i].value.replace(/\s/g,"") == "") {
+				alert("Bitte ein Ende Datum angeben");
+				return;
+			}
+			endedatum = all[i].value;
+			continue;
+		}
+		
+		if(all[i].name == 'startzeit') {
+			if(timePattern.test(all[i].value)) {
+				startzeit = all[i].value;	
+			} else {
+				alert("Bitte eine korrekte Start Zeit angeben (hh:mm:ss)");
+				return;
+			}
+		}
+		
+		
+		if(all[i].name == 'endezeit') {
+			if(timePattern.test(all[i].value)) {
+				endezeit = all[i].value;		
+			} else {
+				alert("Bitte eine korrekte Ende Zeit angeben (hh:mm:ss)");
+				return;
+			}
+		}
+	}
+	
+	if((new Date(endedatum ) - new Date(startdatum )) < 0 ) {
+		alert("Startzeitpunkt liegt nach dem Endzeitpunkt\nBitte korrigieren");
+		return;
+	} else if((new Date(endedatum ) - new Date(startdatum )) == 0) {
+		if(endezeit.split(':')[0] < startzeit.split(':')[0] ||
+				endezeit.split(':')[1] < startzeit.split(':')[1] ||
+				endezeit.split(':')[2] < startzeit.split(':')[2]) {
+					alert("Startzeitpunkt liegt nach dem Endzeitpunkt\nBitte korrigieren");
+					return;
+				}
+	}
+	
+	
+	
+	data['start'] = startdatum + " " + startzeit;
+	data['ende'] = endedatum + " " + endezeit;
+	data['bootsname'] = getParam('bootsname');
+	send(JSON.stringify(data, null, 2));}
+
+function send(message) {
+	$.ajax({
+		type : "POST",
+		url : "trip_infoJSP.jsp",
+		data : {
+			'action' : 'send',
+			'message' : message
+		},
+		dataType : "json",
+		success : function(data) {
+			alert("ÜBERTRAGEN");
+			window.location.href = "trips.jsp?bootsname=" + getParam('bootsname');
+		}
+	});
+	
+}
---
+++ Seapal_JSP/forms/tripsJS.js
@@ -0,0 +1,68 @@
+$(function() {
+	loadTripsTable(getParam('bootsname'));
+});
+
+
+function getParam(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1].replace(/%20/g, ' ');        }
+    }
+    return (false);
+}
+
+function loadTripsTable(name) {
+    request(name);
+}
+
+
+function request(name) {
+    $.ajax({
+
+        type : "POST",
+        url : "trip_infoJSP.jsp",
+        data : {
+            'action' : 'requestTrips',
+            'message' : name
+        },
+        dataType : "json",
+        success : function(data) {
+            var rowsLength = document.getElementById("tripsTable").rows.length - 1;
+            for (var i = rowsLength;i < data.length+rowsLength; i++) {
+                addRow("tripsTable", new Array(data[i].trip_title, data[i].von, data[i].nach, data[i].skipper, data[i].start));
+            };
+        }
+    });
+}
+
+
+function addRow(tableID, values_array) {
+
+    var table = document.getElementById(tableID);
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    row.setAttribute('class', rowCount % 2 == 0 ? "even" : "odd" );
+
+    for (var i = 0; i < values_array.length; i++) {
+        var cell = row.insertCell(i);
+        var element = document.createTextNode(values_array[i]);
+        cell.setAttribute('width', '160px');
+        cell.appendChild(element);
+    }
+
+    var cell = row.insertCell(values_array.length);
+    var element = document.createElement('img');
+    element.setAttribute('src', '../images_css/arrow_left.png');
+    element.setAttribute('width', '40');
+    element.setAttribute('height', '25');
+    element.onclick = function() {
+        window.location.href = "trip_info.jsp?tripname=" + values_array[0] + "&bootsname=" + getParam('bootsname');
+    }
+
+    cell.appendChild(element);
+
+}
